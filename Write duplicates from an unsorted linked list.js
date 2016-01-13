1f600fc1d42879ca959e0c73cd4328ecfbfa8fc6
BKhurshid <josephsun4@gmail.com>
2016-01-13 09:31:54 -0800

    Finished the question. Did not build out a full linked list because it was not the objective.

--- Write duplicates from an unsorted linked list.js
+++ Write duplicates from an unsorted linked list.js
@@ -2,8 +2,8 @@
 
 var Node = function(val) {
     return {
-        value: val
-        next: null; 
+        value: val,
+        next: null
     };
 }
 
@@ -20,23 +20,35 @@ linkedList.prototype.add = function(val) {
 };
 
 linkedList.prototype.removeDuplicates = function() {
+    //storage for whether nodes have repeated
     var check = {};
+    //to make sure the nodeBefore is node before the current 
+    var count = 0;
     var nodeBefore = this.head; 
-    var pause = false; 
     var currentNode = this.head; 
     for (var i = 0; currentNode != null; currentNode = currentNode.next) {
-        if (pause) {
+        if (count === 2) {
             nodeBefore = nodeBefore.next; 
         } else {
-            pause = false; 
+            count++; 
         }
-        if (check[currentNode.value])
+        if (check[currentNode.value] === undefined) {
+            check[currentNode.value] = true;
+        } else {
+            //if the last node is a duplicate
+            if (currentNode.next === null) {
+                //make sure the tail switches to the second last
+                this.tail = nodeBefore;
+            }
+            nodeBefore.next = currentNode.next;
+        }
     }
 };
 
-linkedList.prototype. = function() {
-    
-};
-
-// Write duplicates from an unsorted linked list
+var link = new linkedList('a');
+link.add('b');
+link.add('c');
+link.add('b');
+link.removeDuplicates();
+console.log("link", link)
 
