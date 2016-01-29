a477633699b2ea33d01390605efeac9b73225b92
BKhurshid <josephsun4@gmail.com>
2016-01-29 14:00:01 -0800

    Finished a sort intersect challenge.

---
+++ CCGD.js
@@ -0,0 +1,35 @@
+function helperSort(x, y) {
+    return x - y;
+}
+
+function sortIntersect(f, m) {
+    var commonAges = [];
+    var agesForFemales = {};
+    var agesForMales = {};
+    for (var i = 0; i < f.length; i++) {
+        if (typeof agesForFemales[f[i]] === 'number') {
+            agesForFemales[f[i]] = agesForFemales[f[i]] + 1; 
+        }else {
+            agesForFemales[f[i]] = 1; 
+        }
+    }
+    for (var j = 0; j < m.length; j++) {
+        if (typeof agesForMales[m[j]] === 'number') {
+            agesForMales[m[j]] = agesForMales[m[j]] + 1; 
+        }else {
+            agesForMales[m[j]] = 1; 
+        }
+    }
+
+    for (var property in agesForFemales) {
+        for (var l = 1; (l <= agesForFemales[property]) && (l <=agesForMales[property]); l++ ) {
+            commonAges.push(parseInt(property));
+        }
+    }
+    return commonAges.sort(helperSort).reverse();
+
+}
+
+women = [8500, 8900, 7200, 1000, 4000, 7200, 9];
+men = [100, 7200, 8900, 1000, 7200, 6, 9];
+console.log(sortIntersect(women, men));
